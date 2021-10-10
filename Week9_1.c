bb20ea8195db2335b7888a04a55e3046fd1d324a
NonGP33M <phumrapee.sk@gmail.com>
2021-10-10 21:16:12 +0700

    Week_9

---
+++ Week9_1.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+const char* morse(char a)
+{
+    switch(a)
+    {
+    case 'a':
+        return ".-";
+    case 'b':
+        return "-...";
+    case 'c':
+        return "-.-.";
+    case 'd':
+        return "-..";
+    case 'e':
+        return ".";
+    case 'f':
+        return "..-.";
+    case 'g':
+        return "--.";
+    case 'h':
+        return "....";
+    case 'i':
+        return "..";
+    case 'j':
+        return ".---";
+    case 'k':
+        return "-.-";
+    case 'l':
+        return ".-..";
+    case 'm':
+        return "--";
+    case 'n':
+        return "-.";
+    case 'o':
+        return "---";
+    case 'p':
+        return ".--.";
+    case 'q':
+        return "--.-";
+    case 'r':
+        return ".-.";
+    case 's':
+        return "...";
+    case 't':
+        return "-";
+    case 'u':
+        return "..-";
+    case 'v':
+        return "...-";
+    case 'w':
+        return ".--";
+    case 'x':
+        return "-..-";
+    case 'y':
+        return "-.--";
+    case 'z':
+        return "--..";
+    case '1':
+        return ".----";
+    case '2':
+        return "..---";
+    case '3':
+        return "...--";
+    case '4':
+        return "....-";
+    case '5':
+        return ".....";
+    case '6':
+        return "-....";
+    case '7':
+        return "--...";
+    case '8':
+        return "---..";
+    case '9':
+        return "----.";
+    case '0':
+        return "-----";
+    case ' ':
+        return "/";
+    default:
+        return " ";
+        break;
+    }
+}
+
+int main()
+{
+    char input[120];
+    gets(input);
+    for ( int i = 0 ; i < strlen(input) ; i++)
+    {
+        if(input[i] != ' ')
+        {
+            printf("%s",morse(tolower(input[i])));
+            printf(" ");
+        }
+    }
+    return 0;
+}
